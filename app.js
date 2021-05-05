2574df03e79859760bf05fc5c6dee9d7c62478ba
skwogus98 <skwogus98@gmail.com>
2021-05-06 01:47:26 +0900

    retus 그는 신인가?

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require("body-parser");
 var hostinfo = fs.readFileSync("hostinfo.txt",'utf-8').split(','); // 호스트ip, 포트정보
 var rp = require("request-promise-native");
 const { response } = require('express');
-
+const retus = require("retus");
 const hostname = hostinfo[0];
 const port = hostinfo[1];
 
@@ -34,7 +34,6 @@ app.get('/',function(req, res, next) {
         if(err){
             console.log(err);
         }
-        
         else{
             if(response.statusCode==200){
                 var result = body
@@ -47,27 +46,58 @@ app.get('/',function(req, res, next) {
     console.log(asdf)
     next();
 })
+/*
+function saveSearchData(url){
+    const Data = new Promise((resolve, reject) => {
+        request.get(hospitalSearch,function(err,res,body){
+            if(err){
+                reject(`err => ${err}`)
+            }
+            else {
+         
+                if(res.statusCode == 200) {
+          
+                          var result = body
+          
+                          //console.log(`body data => ${result}`)
+          
+                          var xmlToJson = xml2json.xml2json(result, {compact: true, spaces: 4});
+          
+                          console.log(xmlToJson)
+                          resolve(xmlToJson)
+                 }
+          
+             }
+        })
+    })
+    return Data
+}
+
+*/
+
 //search part
 app.get('/search',function(req, res){
     console.log(req.query)
-    res.send(asdf);
-    //searchData()
+    /*
+    async function loadData(){
+        try{
+            var sendData = await saveSearchData(hospitalSearch);
+            console.log(sendData);
+            res.send(sendData);
+        }
+        catch(error){
+            console.error(error);
+        }
+    }*/
+    const dat = retus(hospitalSearch);
+    var result = dat.body
+    var xmlToJson = xml2json.xml2json(result, {compact: true, spaces: 4});
+    res.send(xmlToJson)
 });
 
 app.get('/test',function(req, res){
     var sendData;
-    function saveData(hospitalSearch){
-        return new Promise((resolve, reject) => {
-            request(hospitalSearch, (err, res, body) =>{
-                if(err) reject(err);
-                else{
-                    if(res.statusCode!=200){
-                    
-                    }
-                }
-            })
-        })
-    }
+    
     console.log(sendData);
 });
 
@@ -91,7 +121,7 @@ request.get(hospitalSearch, (err,res,body)=>{
 
 
 function searchData(){
-    request(hospitalSearch,function(err,res,body){
+    request.get(hospitalSearch,function(err,res,body){
         if(err){
             console.log(`err => ${err}`)
         }
@@ -112,3 +142,5 @@ function searchData(){
          }
     })
 }
+
+
